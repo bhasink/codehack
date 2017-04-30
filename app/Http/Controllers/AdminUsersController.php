c1729ef86b8790e2bc6fe773d55dab3069c8e763
kbeee <bhasink69@gmail.com>
2017-04-30 22:56:02 +0530

    user section working 100 percent except security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -44,7 +44,16 @@ class AdminUsersController extends Controller
     public function store(UserRequest $request)
     {
 
-        $input = $request->all();
+        if(trim($request->password) == ''){
+            $input =$request->except('password');
+        }
+        else{
+            $input = $request->all();
+        }
+
+
+
+
 
         if($file =$request->file('photo_id')){
 
@@ -90,7 +99,12 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
-        return view('admin.users.edit');
+
+        $user = User::findOrFail($id);
+
+        $roles = Role::pluck('name','id')->all();
+
+        return view('admin.users.edit',compact('user','roles'));
     }
 
     /**
@@ -100,9 +114,35 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserRequest $request, $id)
     {
         //
+
+        $user = User::findOrFail($id);
+
+        if(trim($request->password) == ''){
+            $input =$request->except('password');
+        }
+        else{
+            $input = $request->all();
+        }
+
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images',$name);
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+
+
+        }
+
+        $input['password'] = bcrypt($request->id);
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+
     }
 
     /**
